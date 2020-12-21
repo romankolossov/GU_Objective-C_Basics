84206241906770de4149ee8583b5e50c6cfd9a67
romankolossov <rkolossov@gmail.com>
2020-12-21 06:45:00 +0300

    Lesson5 home work task3 complete

--- GU_Obj-C_Base_HW_Les5/BirdFlockNoARCAutoRelPool/BirdFlockNoARCAutoRelPool/main.m
+++ GU_Obj-C_Base_HW_Les5/BirdFlockNoARCAutoRelPool/BirdFlockNoARCAutoRelPool/main.m
@@ -12,40 +12,44 @@
 #import "FlockOfBirds.h"
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Initialising
-        FlockOfBirds *flockOfBirds = [[FlockOfBirds alloc] init];
-
-        Bird *bird1 = [[Bird alloc] initWithNumber:@1];
-        Bird *bird2 = [[Bird alloc] initWithNumber:@2];
-        Bird *bird3 = [[Bird alloc] initWithNumber:@3];
-        Bird *bird4 = [[Bird alloc] initWithNumber:@4];
-        Bird *bird5 = [[Bird alloc] initWithNumber:@5];
-        Bird *bird6 = [[Bird alloc] initWithNumber:@6];
-        
-        NSArray *birds = [[NSArray alloc] initWithObjects:bird1, bird2, bird3, bird4, bird5, bird6, nil];
-        [bird1 release];
-        [bird2 release];
-        [bird3 release];
-        [bird4 release];
-        [bird5 release];
-        [bird6 release];
-        
-        // Initialising
-        NSMutableString *leader = [@"Super Leader" mutableCopy];
-        BirdLeader *birdLeader = [[BirdLeader alloc] initWithLeader: leader];
-        
-        // shows difference between copy and no copy in class BirdLeader
-        [leader replaceOccurrencesOfString:@"Super" withString:@"Ultimate" options:0 range:NSMakeRange(0, leader.length)];
-        
-        // Configure
-        [flockOfBirds configureWithBirdLeader: birdLeader andBirds: birds];
-        [birdLeader release];
-        [birds release];
-
-        NSLog(@"Flock of birds is ready");
-
-        [flockOfBirds release];
-    }
+    NSAutoreleasePool *pool;
+    pool = [NSAutoreleasePool new];
+    
+    // Initialising
+    FlockOfBirds *flockOfBirds = [[FlockOfBirds alloc] init];
+    [flockOfBirds autorelease];
+    
+    Bird *bird1 = [[Bird alloc] initWithNumber:@1];
+    Bird *bird2 = [[Bird alloc] initWithNumber:@2];
+    Bird *bird3 = [[Bird alloc] initWithNumber:@3];
+    Bird *bird4 = [[Bird alloc] initWithNumber:@4];
+    Bird *bird5 = [[Bird alloc] initWithNumber:@5];
+    Bird *bird6 = [[Bird alloc] initWithNumber:@6];
+    [bird1 autorelease];
+    [bird2 autorelease];
+    [bird3 autorelease];
+    [bird4 autorelease];
+    [bird5 autorelease];
+    [bird6 autorelease];
+    
+    NSArray *birds = [[NSArray alloc] initWithObjects:bird1, bird2, bird3, bird4, bird5, bird6, nil];
+    [birds autorelease];
+    
+    // Initialising
+    NSMutableString *leader = [@"Super Leader" mutableCopy];
+    BirdLeader *birdLeader = [[BirdLeader alloc] initWithLeader: leader];
+    [birdLeader autorelease];
+    
+    // shows difference between copy and no copy in class BirdLeader
+    [leader replaceOccurrencesOfString:@"Super" withString:@"Ultimate" options:0 range:NSMakeRange(0, leader.length)];
+    
+    // Configure
+    [flockOfBirds configureWithBirdLeader: birdLeader andBirds: birds];
+    [flockOfBirds autorelease];
+    
+    NSLog(@"Flock of birds is ready");
+    
+    [pool release];
+    
     return 0;
 }
