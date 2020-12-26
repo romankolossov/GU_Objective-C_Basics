1c19baecb707a361080fbd68c488265d664b7cc3
romankolossov <rkolossov@gmail.com>
2020-12-26 18:43:23 +0300

    Lesson7-8 home work started

---
+++ GU_Obj-C_Base_HW_Les7and8/Ping-Pong/Ping-Pong/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Ping-Pong
+//
+//  Created by Роман Колосов on 26.12.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GU_Obj-C_Base_HW_Les7and8/Ping-Pong/Ping-Pong/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Ping-Pong
+//
+//  Created by Роман Колосов on 26.12.2020.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor magentaColor];
+}
+
+
+@end
